c145faf3827050cea4d4dbcbd3df591a8faa8b8f
Faria Quitamba <fariaquitamba5@gmail.com>
2017-10-02 21:56:49 +0100

    Agenda, projecto de aulas complexo Escolar Girassol 2017

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
-var gulp = require('gulp');
-var postcss = require('gulp-postcss');
-gulp.task('postcss', function(){
-	return gulp.src('./style.css')
-		.pipe(postcss([]))
-		.pipe(gulp.dest('./prod'));
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    postcss = require('gulp-postcss');
+ 	autoprefixer = require('autoprefixer');
+ 	nested = require('postcss-nested');
+gulp.task('stream', function () {
+    // Endless stream mode 
+    return watch('css/**/*.css', { ignoreInitial: true })
+   		.pipe(postcss([nested, autoprefixer]))
+        .pipe(gulp.dest('build'));
 });
+ 
