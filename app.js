37a299c4cd0e194324705386db7b36c39ae93a34
Faria Quitamba <fariaquitamba5@gmail.com>
2017-10-02 22:00:29 +0100

    Agenda, projecto de aulas complexo Escolar Girassol 2017

--- app.js
+++
@@ -1,7 +0,0 @@
-var http = require('http'); 
-
-http.createServer(function (req, res){
-	res.write('Funcionando');
-	res.end();
-	
-}).listen(4000);
--- views/novo.php
+++
@@ -1,6 +0,0 @@
-<form class="form-novo" action="index.html" method="post">
-  <input type="text" name="nome" placeholder="Nome">
-  <input type="tel" name="" placeholder="Telemovel">
-  <input type="text" name="" placeholder="Residência">
-  <input type="submit" name="btn-salvar" value="Salvar">
-</form>
